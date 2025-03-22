5bfbe6017b96208a4a2d22a83d42bcfc0f2d4334
Koki Kato <kato@shanord.com>
2025-03-22 12:53:00 +0900

    キーマップとタップダンスの設定を追加
    - QwertyレイヤーとFunctionレイヤーのキーマップを定義
    - 特定のキーに対するタッピングタームを設定
    - 特定のキーに対するホールド設定を追加

---
+++ keyboards/peej/lumberjack/keymaps/gunubin/config.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+
+#define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD_PER_KEY
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_FORCE_HOLD_PER_KEY
+
+#define HOLD_ON_OTHER_KEY_PRESS
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+
+#define VIAL_KEYBOARD_UID {0xBE, 0x78, 0x4B, 0x49, 0x48, 0xB7, 0xA9, 0x77}
+
+/* this is the left upper and left lower key */
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
---
+++ keyboards/peej/lumberjack/keymaps/gunubin/keymap.c
@@ -0,0 +1,120 @@
+/* Copyright 2020 Paul James
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+enum layers {
+  _QWERTY = 0,
+  _FUNCTION
+};
+
+#define FN MO(_FUNCTION)
+
+#define G_SPC LGUI_T(KC_SPC)
+#define A_ENT RALT_T(KC_ENT)
+
+#define C_A LCTL_T(KC_A)
+#define C_MINS LCTL_T(KC_MINS)
+
+#define S_Z LSFT_T(KC_Z)
+#define S_SLSH LSFT_T(KC_SLSH)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty レイヤー
+ * ,-----------------------------------------. ,-----------------------------------------.
+ * |  1   |  2   |  3   |  4   |  5   |      | |      |  6   |  7   |  8   |  9   |  0   |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |  Q   |  W   |  E   |  R   |  T   |      | |      |  Y   |  U   |  I   |  O   |  P   |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |  A   |  S   |  D   |  F   |  G   |      | |      |  H   |  J   |  K   |  L   |  -   |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |  Z   |  X   |  C   |  V   |  B   |      | |      |  N   |  M   |  ,   |  .   |  /   |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |      |      | Alt  | Cmd  | Shift| Space| | Space| Bksp | Entr | Fn   |      |      |
+ * `-----------------------------------------' `-----------------------------------------'
+ */
+[_QWERTY] = LAYOUT_ortho_5x12(
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, _______, _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    C_A,     KC_S,    KC_D,    KC_F,    KC_G, _______, _______, KC_H,    KC_J,    KC_K,    KC_L,    C_MINS,
+    S_Z,     KC_X,    KC_C,    KC_V,    KC_B, _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  S_SLSH,
+    _______, _______, KC_LALT, G_SPC,_______, KC_LSFT, KC_BSPC,_______,A_ENT,   MO(1),   _______, _______
+),
+
+/* Function レイヤー
+ * ,-----------------------------------------. ,-----------------------------------------.
+ * |  !   |  @   |  #   |  $   |  %   |      | |      |  ^   |  &   |  *   |  (   |  )   |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |  !   |  @   |  #   |  $   |  %   |      | |      |  ^   |  &   |  *   |  (   |  )   |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |  +   |  (   |  )   |  =   |  :   |      | |      | Bksp |  +   |  -   |  :   |  ;   |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |  \   |  {   |  }   |  '   |  "   |      | |      |  +   |  _   |  [   |  ]   |  ?   |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |      |      | Bksp | Space|      |      | |      |      | Space|      |      |      |
+ * `-----------------------------------------' `-----------------------------------------'
+ */
+[_FUNCTION] = LAYOUT_ortho_5x12(
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    KC_PLUS, KC_LPRN, KC_RPRN, KC_EQL,  KC_COLN, _______, _______, KC_BSPC, KC_PLUS, KC_MINS, KC_COLN, KC_SCLN,
+    KC_BSLS, KC_LCBR, KC_RCBR, KC_QUOT, KC_DQT,  _______, _______, KC_PLUS, KC_UNDS, KC_LBRC, KC_RBRC, KC_QUES,
+    _______, _______, KC_BSPC, KC_SPC,  _______, _______, _______, _______,  KC_SPC, _______, _______, _______
+)
+
+};
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case S_Z:
+        case S_SLSH:
+            return 150;
+        default:
+            return 200;
+    }
+}
+
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case G_SPC:
+        case A_ENT:
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case G_SPC:
+        case A_ENT:
+            return false;
+        default:
+            return true;
+    }
+}
+
+bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case G_SPC:
+        case A_ENT:
+            return false;
+        default:
+            return true;
+    }
+}
+
